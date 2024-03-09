719c0671856ba6d11bcd95f3f680f1f1ad615beb
Sirin-PS <edelveis.gh@yandex.ru>
2024-03-09 20:12:12 +0300

    Initial project version

---
+++ client_v1/build/CMakeFiles/rdb_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rdb_client.dir/client1.cpp.o"
+  "CMakeFiles/rdb_client.dir/client1.cpp.o.d"
+  "rdb_client"
+  "rdb_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rdb_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server_v1/build/CMakeFiles/rdb_server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rdb_server.
+# This may be replaced when dependencies are built.
